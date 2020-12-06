ac926f19df2a1163cd8196d8e2a509c1c64ad8e7
Valentin <valentinfvazquez@gmail.com>
2020-12-05 23:11:08 -0300

    MyBatis Order DAO insert and getOrderById added. Changes on methods.

--- src/main/java/com/solvd/secondTeamProject/dao/IOrderDAO.java
+++ src/main/java/com/solvd/secondTeamProject/dao/IOrderDAO.java
@@ -2,19 +2,21 @@ package com.solvd.secondTeamProject.dao;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 
+import com.solvd.secondTeamProject.model.Company;
 import com.solvd.secondTeamProject.model.Order;
 
 public interface IOrderDAO {
 
-	@Insert("INSERT INTO order (companies_id, date) VALUES ")
-	Order save(Order g);
+	@Insert("INSERT INTO order (companies_id, date) VALUES (#{com.id}, #{ord.date})")
+	Order save(@Param("ord") Order g, @Param("com") Company c);
 
 	@Select("SELECT * FROM orders WHERE id = #{id}")
 	@ResultMap("OrderResultMap")
-	Order getById(long id);
+	Order getOrderById(long id);
 
 	@Delete("DELETE FROM orders WHERE id = #{id}")
 	void remove(long id);
--- src/main/java/com/solvd/secondTeamProject/dao/mybatis/OrderDAO.java
+++ src/main/java/com/solvd/secondTeamProject/dao/mybatis/OrderDAO.java
@@ -1,20 +1,35 @@
 package com.solvd.secondTeamProject.dao.mybatis;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.solvd.secondTeamProject.dao.ICompanyDAO;
 import com.solvd.secondTeamProject.dao.IOrderDAO;
+import com.solvd.secondTeamProject.model.Company;
 import com.solvd.secondTeamProject.model.Order;
 
 public class OrderDAO implements IOrderDAO{
-
-	public Order save(Order g,long companyId) {
-		// TODO Auto-generated method stub
-		return null;
-		
-	}
+	private Logger log = LogManager.getLogger(CompanyDAO.class);
 
 	@Override
-	public Order getById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Order getOrderById(long id) {
+		try {
+			InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+			IOrderDAO bhDao = sqlSessionFactory.openSession(true).getMapper(IOrderDAO.class);
+			if(bhDao.getOrderById(id) != null)
+				return bhDao.getOrderById(id);
+		} catch (IOException e) {
+			log.error(e);
+		}
+		return new Order();
 	}
 
 	@Override
@@ -23,10 +38,19 @@ public class OrderDAO implements IOrderDAO{
 		
 	}
 
+
 	@Override
-	public Order save(Order g) {
-		// TODO Auto-generated method stub
-		return null;
+	public Order save(Order g, Company c) {
+		try {
+			InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+			IOrderDAO bhDao = sqlSessionFactory.openSession(true).getMapper(IOrderDAO.class);
+			bhDao.save(g, c);
+		} catch (IOException e) {
+			log.error(e);
+		}
+		return g;
 	}
 
 }
