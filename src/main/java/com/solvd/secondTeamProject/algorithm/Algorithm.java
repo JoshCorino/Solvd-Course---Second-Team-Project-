7fcecf9f4ad416c629fb6d740e66a41667dc3414
MelissaTiseira <melissa_tiseira@hotmail.com>
2020-12-07 17:35:24 -0300

    fixed algorithm

--- src/main/java/com/solvd/secondTeamProject/algorithm/Algorithm.java
+++ src/main/java/com/solvd/secondTeamProject/algorithm/Algorithm.java
@@ -6,11 +6,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.solvd.secondTeamProject.App;
-import com.solvd.secondTeamProject.dao.jdbc.CompanyDAO;
-import com.solvd.secondTeamProject.dao.mybatis.WarehouseDAO;
-import com.solvd.secondTeamProject.dao.services.CompanyTransportService;
-import com.solvd.secondTeamProject.dao.services.OrderService;
-import com.solvd.secondTeamProject.dao.services.WarehouseService;
+import com.solvd.secondTeamProject.dao.jdbc.OrderDAO;
 import com.solvd.secondTeamProject.model.Company;
 import com.solvd.secondTeamProject.model.Order;
 import com.solvd.secondTeamProject.model.Product;
@@ -19,25 +15,21 @@ import com.solvd.secondTeamProject.model.Warehouse;
 
 public class Algorithm {
 	private Logger log = LogManager.getLogger(App.class);
+	private OrderDAO oDAO = new OrderDAO();
 	
-	private static WarehouseDAO wDAO = new WarehouseDAO();
-	private static WarehouseService wService = new WarehouseService();
-	private static CompanyTransportService ctService = new CompanyTransportService();
-	private static CompanyDAO cDAO = new CompanyDAO();
-	private static OrderService oService = new OrderService();
-		
-	public List<ResultRepresentation> bestTransports(List<Order> orders, long companyId){
+	public List<ResultRepresentation> bestTransports(List<Order> orders, Company company, List<Warehouse> warehouses){
 		List<ResultRepresentation> bestTransports = new ArrayList<ResultRepresentation>();
 		boolean foundTransport = false;
 		for (Order o : orders) {
-			List<Product> goods = oService.getGoodsByOrderId(o.getId());
+			oDAO.save(o,company);
+			List<Product> goods = o.getGoods();
 			for (Product g : goods) {
 				foundTransport = false;
 				//que warehouse tiene los productos q necesito (todas las warehouses cargadas)
-				for (Warehouse w : wDAO.getAll()) { 
-					if (wService.getGoodsByWarehouseId(w.getId()).contains(g)) {
+				for (Warehouse w : warehouses) { 
+					if (w.getProducts().contains(g)) {
 						//Necesito saber si ese warehouse tiene el tranporte q necesito
-						List<Transport> allowedTransports = wService.getTransportsAllowedByWarehouseId(w.getId());
+						List<Transport> allowedTransports = w.getTransports();
 						for (Transport tw : allowedTransports) {
 							//Me fijo primero en la lista de los q ya use
 							for (ResultRepresentation tb : bestTransports) {
@@ -46,7 +38,7 @@ public class Algorithm {
 									//controlo la capacidad
 									if (tb.getTransport().getCapacity().compareTo(g.getVolume()*g.getQuantity()) >= 0) {
 										tb.getTransport().setCapacity(tb.getTransport().getCapacity()-(g.getVolume()*g.getQuantity()));										
-										tb.addProduct(cDAO.getCompanyById(companyId),g);
+										tb.addProduct(company,g);
 										foundTransport = true;
 										break;
 									}
@@ -54,7 +46,7 @@ public class Algorithm {
 							}
 							//Si no use nada de bestTransports
 							if (!foundTransport) {
-								List<Transport> companyTransports = ctService.getTransportsByCompanyId(companyId);
+								List<Transport> companyTransports = company.getTransports();
 								//Para no agarrar algun transporte que ya use
 								//if (!bestTransports.isEmpty())
 								//	companyTransports.removeAll(bestTransports);
@@ -64,7 +56,7 @@ public class Algorithm {
 										if (tc.getCapacity().compareTo(g.getVolume()*g.getQuantity()) >= 0) {
 											tc.setCapacity(tc.getCapacity()-(g.getVolume()*g.getQuantity()));
 											ResultRepresentation rr = new ResultRepresentation();
-											rr.addProduct(cDAO.getCompanyById(companyId),g);
+											rr.addProduct(company,g);
 											rr.setTransport(tc);
 											bestTransports.add(rr);	
 											foundTransport = true;
@@ -82,7 +74,8 @@ public class Algorithm {
 				}
 			}
 			
-		}	
+		}
+		
 		return bestTransports;
 	}
 }
