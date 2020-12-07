4a1a0fcfd75217dd1e229e68d3d45c5c438f2187
Josh Corino <joshuc98@gmail.com>
2020-12-07 18:05:38 -0300

    OrderService finished

--- src/main/java/com/solvd/secondTeamProject/dao/services/OrderService.java
+++ src/main/java/com/solvd/secondTeamProject/dao/services/OrderService.java
@@ -1,27 +1,34 @@
 package com.solvd.secondTeamProject.dao.services;
 
-import java.util.List;
 
 import com.solvd.secondTeamProject.dao.IOrderDAO;
+import com.solvd.secondTeamProject.dao.IOrderGoodsDAO;
 import com.solvd.secondTeamProject.dao.IProductDAO;
 import com.solvd.secondTeamProject.dao.jdbc.OrderDAO;
+import com.solvd.secondTeamProject.dao.jdbc.OrderGoodsDAO;
 import com.solvd.secondTeamProject.dao.jdbc.ProductDAO;
+import com.solvd.secondTeamProject.model.Company;
 import com.solvd.secondTeamProject.model.Order;
 import com.solvd.secondTeamProject.model.Product;
 
 public class OrderService {
 	IOrderDAO oDAO = new OrderDAO();
 	IProductDAO pDAO = new ProductDAO();
+	IOrderGoodsDAO ogDAO = new OrderGoodsDAO();
 	
-	public Order save(Order c, long companyId) {
-		for(Product p : c.getGoods()) {
+	public Order save(Order o, Company c) {
+		Order saved=  oDAO.save(o,c);
+		for(Product p : o.getGoods()) {
 			pDAO.save(p);
+			ogDAO.relate(saved, p);
 		}
-		return oDAO.save(c,companyId);
+		return saved;
 	}
+	
+	public Order getOrder(long id) {
+		Order result = oDAO.getOrderById(id);
+		result.setGoods(ogDAO.getProductsByOrderId(id));
+		return result;
+	};
 
-	public List<Product> getGoodsByOrderId(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
